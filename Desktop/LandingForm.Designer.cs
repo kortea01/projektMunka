2bf3c1c78eb34ae3d8e606885b3b55b6a7b59639
Korte Aniko <korte.aniko@gmail.com>
2024-05-04 16:14:29 +0200

    Login added, ingredients&menu full CRUD

---
+++ Desktop/LandingForm.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace RestaurantDesktopApp
+{
+    partial class LandingForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LandingForm));
+            this.menuStrip = new System.Windows.Forms.MenuStrip();
+            this.userManagementToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.restaurantMenuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ingredientsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.userPassword = new System.Windows.Forms.TextBox();
+            this.userEmail = new System.Windows.Forms.TextBox();
+            this.loginButton = new System.Windows.Forms.Button();
+            this.registerButton = new System.Windows.Forms.Button();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.loginGroupBox = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.menuStrip.SuspendLayout();
+            this.loginGroupBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip
+            // 
+            this.menuStrip.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.userManagementToolStripMenuItem,
+            this.restaurantMenuToolStripMenuItem,
+            this.ingredientsToolStripMenuItem});
+            this.menuStrip.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip.Name = "menuStrip";
+            this.menuStrip.Size = new System.Drawing.Size(1199, 28);
+            this.menuStrip.TabIndex = 1;
+            this.menuStrip.Text = "menuStrip2";
+            // 
+            // userManagementToolStripMenuItem
+            // 
+            this.userManagementToolStripMenuItem.Enabled = false;
+            this.userManagementToolStripMenuItem.Name = "userManagementToolStripMenuItem";
+            this.userManagementToolStripMenuItem.Size = new System.Drawing.Size(144, 24);
+            this.userManagementToolStripMenuItem.Text = "User management";
+            // 
+            // restaurantMenuToolStripMenuItem
+            // 
+            this.restaurantMenuToolStripMenuItem.Name = "restaurantMenuToolStripMenuItem";
+            this.restaurantMenuToolStripMenuItem.Size = new System.Drawing.Size(188, 24);
+            this.restaurantMenuToolStripMenuItem.Text = "Manage restaurant menu";
+            this.restaurantMenuToolStripMenuItem.Click += new System.EventHandler(this.restaurantMenuToolStripMenuItem_Click);
+            // 
+            // ingredientsToolStripMenuItem
+            // 
+            this.ingredientsToolStripMenuItem.Name = "ingredientsToolStripMenuItem";
+            this.ingredientsToolStripMenuItem.Size = new System.Drawing.Size(155, 24);
+            this.ingredientsToolStripMenuItem.Text = "Manage ingredients";
+            this.ingredientsToolStripMenuItem.Click += new System.EventHandler(this.ingredientsToolStripMenuItem_Click);
+            // 
+            // userPassword
+            // 
+            this.userPassword.Location = new System.Drawing.Point(178, 116);
+            this.userPassword.Name = "userPassword";
+            this.userPassword.PasswordChar = '*';
+            this.userPassword.Size = new System.Drawing.Size(168, 22);
+            this.userPassword.TabIndex = 1;
+            // 
+            // userEmail
+            // 
+            this.userEmail.Location = new System.Drawing.Point(178, 78);
+            this.userEmail.Name = "userEmail";
+            this.userEmail.Size = new System.Drawing.Size(168, 22);
+            this.userEmail.TabIndex = 0;
+            // 
+            // loginButton
+            // 
+            this.loginButton.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.loginButton.Location = new System.Drawing.Point(279, 160);
+            this.loginButton.Name = "loginButton";
+            this.loginButton.Size = new System.Drawing.Size(67, 27);
+            this.loginButton.TabIndex = 2;
+            this.loginButton.Text = "Login";
+            this.loginButton.UseVisualStyleBackColor = true;
+            this.loginButton.Click += new System.EventHandler(this.loginButton_ClickAsync);
+            // 
+            // registerButton
+            // 
+            this.registerButton.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.registerButton.Location = new System.Drawing.Point(190, 159);
+            this.registerButton.Name = "registerButton";
+            this.registerButton.Size = new System.Drawing.Size(83, 28);
+            this.registerButton.TabIndex = 3;
+            this.registerButton.Text = "Register";
+            this.registerButton.UseVisualStyleBackColor = true;
+            this.registerButton.Click += new System.EventHandler(this.registerButton_Click);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.BackColor = System.Drawing.Color.DarkGray;
+            this.linkLabel1.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.linkLabel1.ForeColor = System.Drawing.Color.Transparent;
+            this.linkLabel1.LinkColor = System.Drawing.Color.White;
+            this.linkLabel1.Location = new System.Drawing.Point(187, 209);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(174, 18);
+            this.linkLabel1.TabIndex = 4;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Forgot your password";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.DarkGray;
+            this.label1.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Location = new System.Drawing.Point(44, 82);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 18);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Email";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.DarkGray;
+            this.label2.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label2.Location = new System.Drawing.Point(44, 120);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 18);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Password";
+            // 
+            // loginGroupBox
+            // 
+            this.loginGroupBox.BackColor = System.Drawing.Color.DarkGray;
+            this.loginGroupBox.Controls.Add(this.linkLabel1);
+            this.loginGroupBox.Controls.Add(this.label3);
+            this.loginGroupBox.Controls.Add(this.label2);
+            this.loginGroupBox.Controls.Add(this.userEmail);
+            this.loginGroupBox.Controls.Add(this.loginButton);
+            this.loginGroupBox.Controls.Add(this.label1);
+            this.loginGroupBox.Controls.Add(this.registerButton);
+            this.loginGroupBox.Controls.Add(this.userPassword);
+            this.loginGroupBox.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.loginGroupBox.Location = new System.Drawing.Point(368, 109);
+            this.loginGroupBox.Name = "loginGroupBox";
+            this.loginGroupBox.Size = new System.Drawing.Size(403, 246);
+            this.loginGroupBox.TabIndex = 1;
+            this.loginGroupBox.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.DarkGray;
+            this.label3.Font = new System.Drawing.Font("Verdana", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label3.Location = new System.Drawing.Point(29, 18);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(287, 34);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Login to BellaItalia";
+            // 
+            // LandingForm
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(1199, 799);
+            this.Controls.Add(this.menuStrip);
+            this.Controls.Add(this.loginGroupBox);
+            this.Name = "LandingForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "LandingForm";
+            this.Load += new System.EventHandler(this.LandingForm_Load);
+            this.menuStrip.ResumeLayout(false);
+            this.menuStrip.PerformLayout();
+            this.loginGroupBox.ResumeLayout(false);
+            this.loginGroupBox.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.MenuStrip menuStrip;
+        private System.Windows.Forms.ToolStripMenuItem userManagementToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem restaurantMenuToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ingredientsToolStripMenuItem;
+        private System.Windows.Forms.TextBox userPassword;
+        private System.Windows.Forms.TextBox userEmail;
+        private System.Windows.Forms.Button loginButton;
+        private System.Windows.Forms.Button registerButton;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox loginGroupBox;
+        private System.Windows.Forms.Label label3;
+    }
+}
---
+++ Desktop/LandingForm.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+using System.Net.Http;
+using System.Net;
+
+namespace RestaurantDesktopApp
+{
+    public partial class LandingForm : Form
+    {
+        private string loginApiEndPoint = ReadSettings("endpointUrlLogin");
+        private string userApiEndPoint = ReadSettings("endpointUrlUsers");
+        private HttpClient client;
+
+        public LandingForm()
+        {
+            InitializeComponent();
+            menuStrip.Hide();            
+            client = new HttpClient();
+        }
+
+        private void LandingForm_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void restaurantMenuToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            RestaurantMenuForm restaurantMenuForm = new RestaurantMenuForm();
+            restaurantMenuForm.Show();
+
+        }
+
+        private void ingredientsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            IngredientsForm ingredientsForm = new IngredientsForm();
+            ingredientsForm.Show();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void loginButton_ClickAsync(object sender, EventArgs e)
+        {
+
+            //TODO: call login api, get the bearer token, check user role
+            string username = userEmail.Text;
+            string password = userPassword.Text;
+
+            try
+            {
+                string token = await Login(username, password);                
+                if (!string.IsNullOrEmpty(token))
+                {
+                    MessageBox.Show("Login successful!");
+                    // Here you can navigate to another form or perform any other actions
+                    //Hide the groupbox
+                    loginGroupBox.Hide();
+                    menuStrip.Show();
+                    User user = await GetUserData(token);
+                    if (user != null)
+                    {
+                        MessageBox.Show("Welcome to BellaItalia dear " + user.FirstName + " " + user.LastName);
+                    }                   
+
+                }
+                else
+                {
+                    MessageBox.Show("Login failed. Invalid username or password.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred: {ex.Message}");
+            }            
+        }
+
+        private async Task<string> Login(string username, string password)
+        {
+            var loginData = new { email = username, password = password };
+            var content = new StringContent(Newtonsoft.Json.JsonConvert.SerializeObject(loginData), Encoding.UTF8, "application/json");
+
+            using (var response = await client.PostAsync(loginApiEndPoint, content))
+            {
+                response.EnsureSuccessStatusCode();
+                var responseBody = await response.Content.ReadAsStringAsync();
+                // Assuming your API returns a token in the response
+                // Modify this according to your actual response format
+                dynamic responseData = Newtonsoft.Json.JsonConvert.DeserializeObject(responseBody);
+                return responseData.token;
+            }
+        }
+
+        // Method to get user data after successful login
+        private async Task<User> GetUserData(string token)
+        {
+            client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
+            using (var response = await client.GetAsync(userApiEndPoint))
+            {
+                response.EnsureSuccessStatusCode();
+                var responseBody = await response.Content.ReadAsStringAsync();
+                // Assuming your API returns user data in the response
+                // Modify this according to your actual response format
+                dynamic responseData = Newtonsoft.Json.JsonConvert.DeserializeObject<User>(responseBody);
+                return responseData;
+            }
+        }
+
+        // Define a class to represent your user data
+        private class UserData
+        {
+            // Add properties according to your user data structure
+        }
+
+        private static string ReadSettings(string keyName)
+        {
+            string result = null;
+            try
+            {
+                var value = System.Configuration.ConfigurationSettings.AppSettings;
+
+                result = value[keyName];
+            }
+            catch (ConfigurationException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            return result;
+        }
+
+        private void registerButton_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Please contact your Restaurant manager for further details");
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            MessageBox.Show("This functionality is under development");
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+
+
+}
